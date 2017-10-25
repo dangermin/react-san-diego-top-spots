0f6f0af55a097bc98cdadde0a27119d50650d06a
Dangermin <dangermin.field@gamil.com>
2017-10-24 17:29:38 -0700

    All test pass

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 export default props => (
-  <div className='well' >
-    <h4>{props.name}</h4>
-    <h4>{props.description}</h4>
+  <div >
+      <div className="well">
+          <h4 >{props.name}</h4>
+          <p >{props.description}</p>
+          <a className="btn btn-primary" type="button" href={"https://maps.google.com/?q="+props.location[0]+","+props.location[1]} target="_blank">Location</a>
+      </div>
   </div>
 );
